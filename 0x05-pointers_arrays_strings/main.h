dc35e422c47f02ec86dfe07673d51713c71d93f0
Ugochukwu C Nwosu <111017752+Ugobarca@users.noreply.github.com>
2022-09-20 02:47:47 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * void prototypes()
- * int prototypes()
- */
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,8 +6,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void _putchar(char);
 void print_array(int *a, int n);
-char *_strepy(char *dest, char *src);
-int _atoi(char *str);
-
-#endif /*MAIN_H*/
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);

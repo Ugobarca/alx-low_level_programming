3d6c21776aecba689aeda3c25a93cc4034922c26
Ugochukwu C Nwosu <111017752+Ugobarca@users.noreply.github.com>
2022-09-27 00:27:52 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * *_memset - concatenates two strings
- * @s:  pointer
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: mamory area to return
  * @b: constant byte
- * @n: n bytes of memory
- * Return: s
+ * @n: size of bytes
+ * Return: char
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; n > 0 ; i++, n--)
+	while (i < n)
 	{
-		s[i] = b;
-	}
 
+		*(s + i) = b;
+		i += 1;
+	}
 	return (s);
 }
